161c47d21b4d3eaa3be085cc1e8a55b350213482
Hecker_01 <101524930+hecker-01@users.noreply.github.com>
2025-05-20 12:24:30 +0200

    feat: add Quoter tab to Header component navigation

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,6 +5,7 @@
     { name: "Projects ", link: "/#projects" },
     { name: "About me", link: "/#about" },
     { name: "Contact", link: "/contact" },
+    { name: "Quoter", link: "/quoter" },
   ];
 </script>
 
